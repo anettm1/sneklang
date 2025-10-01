3d4a0269ef750a9c782c7fcd0f122c69d5ff27fa
anettm1 <anettmaior@gmail.com>
2025-10-01 15:32:28 +0100

    feat(float): add float type

--- main.c
+++ main.c
@@ -6,5 +6,8 @@ int main(){
     snek_object_t* num = new_snek_integer(2);
     printf("%d\n", num->data.v_int);
     free(num);
+
+    snek_object_t* float_num = new_snek_float(3.5);
+    printf("%.2f\n", float_num->data.v_float);
     return 0;
 }
--- snekobject.c
+++ snekobject.c
@@ -8,3 +8,11 @@ snek_object_t *new_snek_integer(int value){
     obj->data.v_int = value;
     return obj;
 }
+
+snek_object_t *new_snek_float(float value){
+    snek_object_t *obj = malloc(sizeof(snek_object_t));
+    if(obj == NULL){return NULL;}
+    obj->kind = FLOAT;
+    obj->data.v_float = value;
+    return obj;
+}
